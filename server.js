0733da9ce62631225c1f5dce1720a7401472fafc
sachipatel <sachixl300@gmail.com>
2026-04-24 14:11:49 +0530

    backend port change

--- server.js
+++ server.js
@@ -20,7 +20,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://sachipatel-farm-advisory-back.onrender.com",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
